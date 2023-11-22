ea790531251298a134f19dbfbe1ffe18983f51c4
jammoul271 <jammoul27@gmail.com>
2023-11-22 16:14:22 +0100

    Socket error fix

--- src/mqtt_bridge/app.py
+++ src/mqtt_bridge/app.py
@@ -4,11 +4,11 @@ from __future__ import absolute_import
 import inject
 import paho.mqtt.client as mqtt
 import rospy
+import threading
 
 from .bridge import create_bridge
 from .mqtt_client import create_private_path_extractor
 from .util import lookup_object
-
 
 def create_config(mqtt_client, serializer, deserializer, mqtt_private_path):
     if isinstance(serializer, basestring):
@@ -26,47 +26,62 @@ def create_config(mqtt_client, serializer, deserializer, mqtt_private_path):
     return config
 
 
+def mqtt_thread(mqtt_client):
+    try:
+        # load parameters
+        params = rospy.get_param("~", {})
+        mqtt_params = params.pop("mqtt", {})
+        conn_params = mqtt_params.pop("connection")
+        mqtt_private_path = mqtt_params.pop("private_path", "")
+        bridge_params = params.get("bridge", [])
+
+        # load serializer and deserializer
+        serializer = params.get("serializer", "json:dumps")
+        deserializer = params.get("deserializer", "json:loads")
+
+        # dependency injection
+        config = create_config(mqtt_client, serializer, deserializer, mqtt_private_path)
+        inject.configure(config)
+
+        # Configure and connect to MQTT broker
+        mqtt_client.on_connect = _on_connect
+        mqtt_client.on_disconnect = _on_disconnect
+        mqtt_client.connect(**conn_params)
+        mqtt_client.sub_topics = []
+
+        # configure bridges
+        bridges = []
+        for bridge_args in bridge_params:
+            bridges.append(create_bridge(**bridge_args))
+            if bridge_args["factory"] == "mqtt_bridge.bridge:MqttToRosBridge":
+                mqtt_client.sub_topics.append(bridge_args["topic_from"])
+
+        # Start the MQTT client loop
+        mqtt_client.loop_forever()
+
+    except Exception as e:
+        print("Exception in MQTT thread: {}".format(e))
+        rospy.signal_shutdown(e)
+
+
 def mqtt_bridge_node():
     # init node
     rospy.init_node("mqtt_bridge_node")
 
-    # load parameters
+
     params = rospy.get_param("~", {})
     mqtt_params = params.pop("mqtt", {})
-    conn_params = mqtt_params.pop("connection")
-    mqtt_private_path = mqtt_params.pop("private_path", "")
-    bridge_params = params.get("bridge", [])
-
-    # create mqtt client
+        
+    # Create mqtt client
     mqtt_client_factory_name = rospy.get_param(
         "~mqtt_client_factory", ".mqtt_client:default_mqtt_client_factory"
     )
     mqtt_client_factory = lookup_object(mqtt_client_factory_name)
     mqtt_client = mqtt_client_factory(mqtt_params)
-
-    # load serializer and deserializer
-    serializer = params.get("serializer", "json:dumps")
-    deserializer = params.get("deserializer", "json:loads")
-
-    # dependency injection
-    config = create_config(mqtt_client, serializer, deserializer, mqtt_private_path)
-    inject.configure(config)
-
-    # Configure and connect to MQTT broker
-    mqtt_client.on_connect = _on_connect
-    mqtt_client.on_disconnect = _on_disconnect
-    mqtt_client.connect(**conn_params)
-    mqtt_client.sub_topics = []
-
-    # configure bridges
-    bridges = []
-    for bridge_args in bridge_params:
-        bridges.append(create_bridge(**bridge_args))
-        if bridge_args["factory"] == "mqtt_bridge.bridge:MqttToRosBridge":
-            mqtt_client.sub_topics.append(bridge_args["topic_from"])
-
-    # start MQTT loop
-    mqtt_client.loop_start()
+        
+    # Start the MQTT client loop in a separate thread
+    mqtt_thread_instance = threading.Thread(target=mqtt_thread,args=(mqtt_client,))
+    mqtt_thread_instance.start()
 
     # register shutdown callback and spin
     rospy.on_shutdown(mqtt_client.disconnect)
